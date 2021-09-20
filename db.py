3ea8d8cf33af76f7320e293c8fe1ad622daa63b9
Loki <loki@example.com>
2021-09-20 10:55:19 -0400

    Added pylint

--- db.py
+++ db.py
@@ -5,13 +5,11 @@ Database class for the bot.
 """
 
 import logging
-from sqlite3 import OperationalError
-from typing import List
+from sqlite3 import OperationalError # pylint: disable=E0611
 
 import aiosqlite
 
-from core.settings import DB_NAME, LANG, LOG_LEVEL, LOG_FORMAT, LOG_DATE_FORMAT
-from core.utils import loginfo, logwarn, logcritical
+from core.settings import DB_NAME, LOG_LEVEL, LOG_FORMAT, LOG_DATE_FORMAT
 
 
 logger = logging.getLogger(__name__)
@@ -24,61 +22,64 @@ logger.addHandler(handler)
 
 
 class DB:
-    """Represents a sqlite3 database instance
+    """
+    Represents a sqlite3 database instance
 
     The `DB` class is used to implement some level of persistence, the database
     name can be modified with the `DB_NAME`
 
     WARNING: Some methods may be suceptible to SQL Injection, the methods of
     this class should never accept user input directly without doing some clean
-    up first 
-    
+    up first
     """
-    
+
     @classmethod
-    async def select(cls, columns: List[str], table: str) -> aiosqlite.Cursor:
+    async def select(cls, columns, table):
         """
         Selects columns from a database
         """
         columns = ",".join(columns)
-        async with aiosqlite.connect(DB_NAME) as db:
-            loginfo(logger, f'Selecting from database {table}: {columns}')
-            async with db.execute(f'SELECT {columns} FROM {table}') as cursor:
-                return cursor
-    
+        async with aiosqlite.connect(DB_NAME) as cursor:
+            logger.info('Selecting from database %s: %s', table, columns)
+            async with cursor.execute(f'SELECT {columns} FROM {table}') as _cursor:
+                return _cursor
+
     @classmethod
-    async def selectWhere(cls, columns: List[str], table: str, where: str) -> aiosqlite.Cursor:
+    async def select_where(cls, columns, table, where):
         """
         Selects columns from a database where the values match the `where`
         expression
         """
         columns = ",".join(columns)
-        async with aiosqlite.connect(DB_NAME) as db:
-            loginfo(logger, f'Selecting from database {table}: {columns}')
-            async with db.execute(f'SELECT {columns} FROM {table} WHERE {where}') as cursor:
-                return cursor
+        async with aiosqlite.connect(DB_NAME) as cursor:
+            logger.info('Selecting from database %s: %s', table, columns)
+            async with cursor.execute(
+                    f'SELECT {columns} FROM {table} WHERE {where}') as _cursor:
+                return _cursor
 
     @classmethod
-    async def insert(cls, into_table: str, columns: List[str], values: List[str]) -> None:
+    async def insert(cls, into_table, columns, values):
         """Insert values into a table"""
         columns = ",".join(columns)
         values = ",".join(values)
-        async with aiosqlite.connect(DB_NAME) as db:
-            loginfo(logger, f'Inserting into database {into_table} values {values}')
-            await db.execute(f'INSERT INTO {into_table} ({columns}) VALUES ({values})')
-            await db.commit()
-    
+        async with aiosqlite.connect(DB_NAME) as cursor:
+            logger.info(
+                'Inserting into database %s values %s', into_table, values)
+            await cursor.execute(
+                f'INSERT INTO {into_table} ({columns}) VALUES ({values})')
+            await cursor.commit()
+
     @classmethod
-    async def _create(cls, table: str, columns: List[str]) -> None:
+    async def _create(cls, table, columns):
         """Creates a table with the given columns"""
         columns = ",".join(columns)
-        async with aiosqlite.connect(DB_NAME) as db:
-            loginfo(logger, f'Creating database table: {table}')
-            await db.execute(f'CREATE TABLE {table} ({columns})')
+        async with aiosqlite.connect(DB_NAME) as cursor:
+            logger.info('Creating database table: %s', table)
+            await cursor.execute(f'CREATE TABLE {table} ({columns})')
 
 
     @classmethod
-    async def setup(cls) -> None:
+    async def setup(cls):
         """Checks if a database exists and if it has the basic configuration"""
         try:
             pass
