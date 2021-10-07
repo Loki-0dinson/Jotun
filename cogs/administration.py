e5b377a6ea6bdce73540beb488e4aefe8b2ef1ab
Loki-0dinson <loki@example.com>
2021-10-07 19:23:06 -0400

    Basic version of kick and ban commands #2

--- cogs/administration.py
+++ cogs/administration.py
@@ -6,7 +6,6 @@ Administrative commands
 
 from typing import TypeVar
 from nextcord.ext import commands
-from nextcord.ext.commands import has_permissions, Context, Bot
 from nextcord.ext.commands.errors import CommandError, MissingPermissions
 from nextcord.member import Member
 
@@ -16,37 +15,49 @@ _CE_contra = TypeVar('_CE_contra', bound='CommandError', contravariant=True)
 
 class Administration(commands.Cog):
 
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command()
-    @has_permissions(kick_members=True)
-    async def kick(self, ctx: Context, user: Member = None, reason: str = ''):
-        await self.bot.kick(user, reason=reason)
-        await ctx.send((user, reason))
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx: commands.Context, user: Member = None, *args):
+        _user = {
+            'name': f'{user.name}#{user.discriminator}',
+            'id': f'({user.id})'
+        }
+        reason = 'No reason provided' if not args else ' '.join(args)
+        await user.kick(reason=reason)
+        await ctx.send(f'User: `{_user["name"]}` `{_user["id"]}`\
+            \nReason: {reason}')
 
     @kick.error
-    async def kick_error(error: _CE_contra, ctx: Context):
+    async def kick_error(error: _CE_contra, ctx: commands.Context):
         if isinstance(error, MissingPermissions):
             await ctx.send("You don't have permission to do that!")
         else:
             await ctx.send(error, type(error))
 
-    @commands.command
-    @has_permissions(ban_members=True)
-    async def ban(self, ctx: Context, user: Member = None, reason: str = ''):
-        await self.bot.ban(user, reason=reason)
-        await ctx.send((user, reason))
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx: commands.Context, user: Member = None, *args):
+        _user = {
+            'name': f'{user.name}#{user.discriminator}',
+            'id': f'({user.id})'
+        }
+        reason = 'No reason provided' if not args else ' '.join(args)
+        await user.ban(reason=reason)
+        await ctx.send(f'User: `{_user["name"]}` `{_user["id"]}`\
+            \nReason: {reason}')
 
-    @kick.error
-    async def ban_error(error: _CE_contra, ctx: Context):
+    @ban.error
+    async def ban_error(error: _CE_contra, ctx: commands.Context):
         if isinstance(error, MissingPermissions):
             await ctx.send("You don't have permission to do that!")
         else:
             await ctx.send(error, type(error))
 
 
-def setup(bot: Bot):
+def setup(bot: commands.Bot):
     """Initializes and adds the cog to the bot
 
     This function gets called by `load_extension` in order to load the Cog
