b57e1a50b400896054597b12a16352fd790bbe52
Loki-0dinson <loki@example.com>
2021-10-07 21:43:09 -0400

    Enhanced kick and ban with embeds

--- cogs/administration.py
+++ cogs/administration.py
@@ -5,9 +5,12 @@ Administrative commands
 """
 
 from typing import TypeVar
+from nextcord.embeds import Embed
 from nextcord.ext import commands
 from nextcord.ext.commands.errors import CommandError, MissingPermissions
 from nextcord.member import Member
+
+from core.settings import COLOR_ERROR, COLOR_SUCCESS
 
 
 _CE_contra = TypeVar('_CE_contra', bound='CommandError', contravariant=True)
@@ -21,14 +24,19 @@ class Administration(commands.Cog):
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx: commands.Context, user: Member = None, *args):
-        _user = {
-            'name': f'{user.name}#{user.discriminator}',
-            'id': f'({user.id})'
-        }
+        if not user:
+            eb = Embed(colour=COLOR_ERROR, title='Missing user!',
+                description='Usage: `$kick [@user or id] [reason]`')
+            return await ctx.send(embed=eb)
+
         reason = 'No reason provided' if not args else ' '.join(args)
+
         await user.kick(reason=reason)
-        await ctx.send(f'User: `{_user["name"]}` `{_user["id"]}`\
-            \nReason: {reason}')
+        eb = Embed(colour=COLOR_SUCCESS, title='User kicked',
+            description=f'User: `{user.name}#{user.discriminator}` \
+                `({user.id})`\nReason: {reason}'
+        )
+        await ctx.send(embed=eb)
 
     @kick.error
     async def kick_error(error: _CE_contra, ctx: commands.Context):
@@ -40,14 +48,19 @@ class Administration(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx: commands.Context, user: Member = None, *args):
-        _user = {
-            'name': f'{user.name}#{user.discriminator}',
-            'id': f'({user.id})'
-        }
+        if not user:
+            eb = Embed( colour=COLOR_ERROR, title='Missing user!',
+                description='Usage: `$ban [@user or id] [reason]`')
+            return await ctx.send(embed=eb)
+
         reason = 'No reason provided' if not args else ' '.join(args)
+
         await user.ban(reason=reason)
-        await ctx.send(f'User: `{_user["name"]}` `{_user["id"]}`\
-            \nReason: {reason}')
+        eb = Embed(colour=COLOR_SUCCESS, title='User banned',
+            description=f'User: `{user.name}#{user.discriminator}` \
+                `({user.id})`\nReason: {reason}'
+        )
+        await ctx.send(embed=eb)
 
     @ban.error
     async def ban_error(error: _CE_contra, ctx: commands.Context):
