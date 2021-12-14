e901079715499e976413dab2de2d5a409b5309fa
Loki <loki@example.com>
2021-12-14 09:51:16 -0400

    custom command parser test

--- cogs/administration.py
+++ cogs/administration.py
@@ -17,21 +17,48 @@ _CE_contra = TypeVar('_CE_contra', bound='CommandError', contravariant=True)
 
 
 class Administration(commands.Cog):
+    """Moderation and server administration commands"""
+    qualified_name = 'Administration'
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kick(self, ctx: commands.Context, user: Member = None, *args):
-        if not user:
-            eb = Embed(colour=COLOR_ERROR, title='Missing user!',
-                description='Usage: `$kick [@user or id] [reason]`')
-            return await ctx.send(embed=eb)
+    async def kick(self, ctx: commands.Context, *args):
+        """Kicks an user or group of users works with both mentions or user id
 
-        reason = 'No reason provided' if not args else ' '.join(args)
+            `-u` : specify single user
+            `-U` : specify ultiple users
+            `-r` : Specify reason (must use quotation marks)
 
-        await user.kick(reason=reason)
+        Examples:
+            `$kick @user_or_id`
+            `$kick @user1 @user2`
+            `$kick -u @user`
+            `$kick -U @user1 @user2`
+            `$kick -u @user -r "reason for kicking"`
+            `$kick -U @user1 @user2 -r "reason for kicking"`
+        """
+
+        # TODO: delet dis, use shlex + argparse
+        match args:
+            case ['-u', user]:
+                await Member(user).kick(reason='No reason provided')
+
+            case ['-U', *users, '-r', reason] | ['-r', reason, '-U', *users]:
+                for user in users:
+                    await Member(user).kick(reason=reason)
+
+            case ['-u', user, '-r', reason] | [user, reason] | ['-r', reason, '-u', user]:
+                await Member(user).kick(reason=reason)
+
+            case ['-U', *users] | [*users]:
+                for user in users:
+                    await Member(user).kick(reason='No reason provided')
+            case _:
+                raise CommandError()
+
         eb = Embed(colour=COLOR_SUCCESS, title='User kicked',
             description=f'User: `{user.name}#{user.discriminator}` \
                 `({user.id})`\nReason: {reason}'
@@ -39,13 +66,17 @@ class Administration(commands.Cog):
         await ctx.send(embed=eb)
 
     @kick.error
-    async def kick_error(error: _CE_contra, ctx: commands.Context):
+    async def kick_error(error: _CE_contra, ctx: commands.Context, *args):
         if isinstance(error, MissingPermissions):
             await ctx.send("You don't have permission to do that!")
         else:
-            await ctx.send(error, type(error))
+            eb = Embed(colour=COLOR_ERROR, title='Command error')
+            eb.add_field(name='Example 1', value='$kick -u @user_or_id')
+            eb.add_field(name='Example 2', value='$kick -U @user1 @user2')
+            eb.add_field(name='Example 3', value='$kick -u @user -r "reason for kicking"')
+            await ctx.send(embed=eb)
 
-    @commands.command()
+    @commands.command(help='Bans an user or group of users')
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx: commands.Context, user: Member = None, *args):
         if not user:
